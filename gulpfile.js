03483226835351a861161d585c0e4caaf2e5f116
Uri Shaked <uri@salsa4fun.co.il>
2014-05-21 18:17:42 +0300

    Streamline development using gulp (livereload + sass)

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+/* Gulpfile for NG-CON Israel Website */
+/* Provides SASS + Livereload functions */
+/* Copyright (C) 2014, Uri Shaked. License: ISC */
+
+var gulp = require('gulp'),
+	watch = require('gulp-watch'),
+	sass = require('gulp-sass'),
+	livereload = require('gulp-livereload'),
+	debug = require('gulp-debug'),
+	connectLivereload = require('connect-livereload'),
+	express = require('express');
+
+var serverPort = 7000;
+var livereloadPort = 35730;
+
+var paths = {
+	images: ['images/*.{svg,png}'],
+	styles: ['styles/*.scss'],
+	html: ['*.html']
+};
+
+gulp.task('sass', function() {
+	gulp.src(paths.styles)
+		.pipe(sass())
+		.pipe(gulp.dest('styles'))
+});
+
+gulp.task('serve', ['sass'], function () {
+	var server = express();
+	server.use(connectLivereload({
+		port: livereloadPort
+	}));
+	server.use(express.static('.'));
+	server.listen(serverPort);
+});
+
+gulp.task('watch', function () {
+	var lrserver = livereload(livereloadPort);
+
+	gulp.src(paths.styles)
+		.pipe(watch())
+		.pipe(sass())
+		.pipe(gulp.dest('styles'))
+		.pipe(lrserver);
+
+	gulp.src([].concat(paths.images, paths.html))
+		.pipe(watch())
+		.pipe(lrserver);
+});
+
+gulp.task('default', ['serve', 'watch']);
