bd0de6a084bdcefa2a98b4e8b992c6347f3c582d
oshri@widexposure.co.il <interact@widexposure.co.il>
2014-05-23 10:23:54 +0300

    Change home section and some more...

---
+++ js/header.js
@@ -0,0 +1,25 @@
+/**
+ * Created by oshrikdoshim on 5/22/14.
+ */
+
+(function(window, document){
+
+    var header = document.getElementsByClassName('headerSection');
+
+
+
+
+    var init = function(){
+        headerSection();
+    };
+
+    var headerSection = function(){
+//        header[0].style.visibility='hidden';
+
+    };
+
+    window.onload=function() {
+        init();
+    };
+
+})(window, document);
